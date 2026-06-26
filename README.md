ce969e8f47672016aa010e0facbe1793c1bd802a
Ross Curley <ross.curley@outlook.com>
2026-06-26 16:43:40 +0800

    Add syslinux configuration for boot options
    - Introduced a new syslinux.cfg file to provide boot options for the system.
    - Added labels for 'select', 'pxe', and 'sys' with corresponding configurations.
    - Enabled selection of boot methods including PXE and ISO.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # README
+
+## Build Tools
+```sh
+pacman -Syu --noconfirm
+pacman -S archiso --noconfirm
+```
+## Build ISO
+```sh
+cd /workspace/profile
+mkarchiso -v -w /tmp/archiso-tmp -o /workspace/_out/ .
+```
--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
-# License: GPL-3.0-or-later
+# shellcheck disable=SC2034
 
-iso_name="mycustomlinux"
-iso_label="MY_CUSTOM_ARCH_$(date +%Y%m)"
-iso_publisher="YourName <https://github.com/yourusername>"
-iso_application="Custom Arch Linux Live/Installation CD"
-iso_version="$(date +%Y.%m.%d)"
+iso_name="ArchNet"
+iso_label="ARCH_NET_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Arch Net <https://github.com/sailfact>"
+iso_application="Arch Net Live/Install CD"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.grub.esp' 'uefi-x64.grub.esp' 'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
-
-# File permissions for the live ISO environment
+bootmodes=('bios.syslinux'
+           'uefi.systemd-boot')
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
 )
